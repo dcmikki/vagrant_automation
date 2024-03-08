903e380bb7b187db2306af0ca02f6b16e7ba7d78
dcmikki <dcmikki@gmail.com>
2024-03-08 03:13:43 +0000

    upgrade 2024

--- libvirt/centos7-minikube/initiate_minikube.sh
+++ libvirt/centos7-minikube/initiate_minikube.sh
@@ -2,7 +2,9 @@
 
 # Initiate minikube as vagrant using docker provider
 #  minikube start --driver=kvm2 --cpus=4 --memory=4400mb --disk-size=10000mb
-minikube start --driver=docker --cpus=4 --memory=2200mb
+minikube start --driver=docker --cpus=4 --memory=4400mb
+
+sleep 5
 
 # Kubernetes Service Cluster
 kubectl get po -A
