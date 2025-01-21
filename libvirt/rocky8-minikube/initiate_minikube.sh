3e456a246ad5828ba12dc4169f1ef0a5f00d2f02
dcmikki <dcmikki@gmail.com>
2025-01-21 00:12:27 +0000

    add rocky8 minikube

---
+++ libvirt/rocky8-minikube/initiate_minikube.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Initiate minikube as vagrant using docker provider
+#  minikube start --driver=kvm2 --cpus=4 --memory=4400mb --disk-size=10000mb
+minikube start --driver=docker --cpus=4 --memory=6500mb
+
+sleep 10
+
+# Kubernetes Service Cluster
+kubectl get po -A
